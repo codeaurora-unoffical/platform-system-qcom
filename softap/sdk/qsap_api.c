158d6f7e879d6185537a719bade5b0999e3a4dfe
Mahesh A Saptasagar <msapta@codeaurora.org>
2013-12-03 16:35:15 +0530

    wlan: Don't overwrite the channel configured in hostapd.conf
    When SAP is turned on through GUI, hostapd.conf file
    will be overwritten by the framework parameters.Since
    in Kit-Kat framework,channel value is hard coded to 6,
    same is getting over written whenever we turn on SAP
    through GUI, this will cause SAP to come up always on
    channel 6.
    To mitigate this issue, avoid overwriting the channel
    parameter in hostap.conf file by ignoring the channel
    parameter from the framework.
    
    Change-Id: I4d87cf70868ad705f63211b2bd10ac2a67646d09
    CRs-Fixed: 584112

--- softap/sdk/qsap_api.c
+++ softap/sdk/qsap_api.c
@@ -3089,6 +3089,7 @@ int qsapsetSoftap(int argc, char *argv[])
             return -1;
         }
     }
+#ifndef ANDROID_VERSION_KITKAT
     /** channel */
     rlen = RECV_BUF_LEN;
     if(argc > 5) {
@@ -3100,7 +3101,7 @@ int qsapsetSoftap(int argc, char *argv[])
             return -1;
         }
     }
-
+#endif
     /** Security */
     rlen = RECV_BUF_LEN;
     if(argc > 6) {
