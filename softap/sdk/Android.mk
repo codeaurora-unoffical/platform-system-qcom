f391791f8b35e17c7eea1338b6212d886386df65
Nagaraj <nanna@codeaurora.org>
2014-12-16 16:07:47 -0800

    softap: Add support for STA+P2PGO+SAP Concurrency
    1. Add functions to create and remove softap interface using
       NL commands.
    2. Write Softap interface into hostapd.conf.
    
    Change-Id: I77727424719aada35fa8ea84d35fed8a5edfb49f
    CRs-Fixed: 741484

--- softap/sdk/Android.mk
+++ softap/sdk/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES :=
+LOCAL_C_INCLUDES := external/libnl/include external/wpa_supplicant_8/wpa_supplicant/src/drivers
 
 LOCAL_MODULE:= libqsap_sdk
 
@@ -61,7 +61,7 @@ LOCAL_SRC_FILES := qsap_api.c \
 
 LOCAL_PRELINK_MODULE := false
 
-LOCAL_SHARED_LIBRARIES := libnetutils libutils libbinder libcutils
+LOCAL_SHARED_LIBRARIES := libnetutils libutils libbinder libcutils libnl
 
 include $(BUILD_SHARED_LIBRARY)
 
